edac3cff195321ac5147e99c005fc61637f5a667
Taylor Dunn <58095194+TailorDone@users.noreply.github.com>
2020-04-14 11:35:22 -0600

    Eleven Made

--- src/a10/Eleven.java
+++ src/a10/Eleven.java
@@ -1,5 +1,47 @@
 package a10;
 
-public class Eleven {
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Eleven extends Plant{
+
+	
+
+	private static final int HEALTH = 200;
+	private static final int COOLDOWN = 20;
+	private static final int ATTACKDMG = 100;
+	private static final BufferedImage IMAGE;
+	static {
+		BufferedImage localImage = null;
+		try {
+			localImage = ImageIO.read(new File("src/a10/Icons/eleven.png"));
+
+		} catch (IOException e) {
+			System.out.println("Eleven image was not found");
+			System.exit(0);
+		}
+		IMAGE = localImage;
+	}
+	
+	public Eleven(Double startingPosition) {
+		super(startingPosition, new Point2D.Double(IMAGE.getWidth(), IMAGE.getHeight()), IMAGE, HEALTH, COOLDOWN, ATTACKDMG);
+		
+	}
+	
+	@Override
+	public void attack(Actor other) {
+		if (other instanceof Zombie && this != other && this.isCollidingOther(other)) {
+			if(this.readyForAction()) {
+				other.changeHealth(-ATTACKDMG);
+				this.resetCoolDown();
+			}
+			
+		}
+	}
 
 }
