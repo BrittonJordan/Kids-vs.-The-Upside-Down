86cb0cf9a3646b9a13bf94f56622f8f3abf65460
Taylor Dunn <58095194+TailorDone@users.noreply.github.com>
2020-04-14 11:46:16 -0600

    Update Will.java

--- src/a10/Will.java
+++ src/a10/Will.java
@@ -1,5 +1,43 @@
 package a10;
 
-public class Will {
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+public class Will extends Plant{
+
+	private static final int HEALTH = 100;
+	private static final int COOLDOWN = 40;
+	private static final int ATTACKDMG = 20;
+	private static final BufferedImage IMAGE;
+	static {
+		BufferedImage localImage = null;
+		try {
+			localImage = ImageIO.read(new File("src/a10/Icons/will.png"));
+
+		} catch (IOException e) {
+			System.out.println("Will image was not found");
+			System.exit(0);
+		}
+		IMAGE = localImage;
+	}
+	
+	public Will(Double startingPosition) {
+		super(startingPosition, new Point2D.Double(IMAGE.getWidth(), IMAGE.getHeight()), IMAGE, HEALTH, COOLDOWN, ATTACKDMG);
+	}
+	
+		
+	@Override
+	public void attack(Actor other) {
+		if (other instanceof Zombie && this != other && this.isCollidingOther(other)) {
+			if(this.readyForAction()) {
+				other.changeHealth(-ATTACKDMG);
+				this.resetCoolDown();
+			}	
+		}
+	}
 }
