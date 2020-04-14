b25efe1438f7704006f1c003977176a9d145d341
Taylor Dunn <58095194+TailorDone@users.noreply.github.com>
2020-04-14 11:23:41 -0600

    Abstract Plant

--- src/a10/Plant.java
+++ src/a10/Plant.java
@@ -4,7 +4,7 @@ import java.awt.geom.Point2D;
 import java.awt.geom.Point2D.Double;
 import java.awt.image.BufferedImage;
 
-public class Plant extends Actor {
+public abstract class Plant extends Actor {
 	
 	public Plant(Point2D.Double startingPosition, Point2D.Double initHitbox, BufferedImage img, int health, int coolDown, int attackDamage) {
 		super(startingPosition, initHitbox, img, health, coolDown, 0, attackDamage);
@@ -19,8 +19,5 @@ public class Plant extends Actor {
 	 * @param other
 	 */
 	@Override
-	public void attack(Actor other) {
-		if (other instanceof Zombie)
-			super.attack(other);
-	}
+	public abstract void attack(Actor other);
 }
