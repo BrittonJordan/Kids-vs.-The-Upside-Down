9eeef237abab5940a9534a69610056839f8242a2
Taylor Dunn <58095194+TailorDone@users.noreply.github.com>
2020-04-14 11:44:28 -0600

    Update Mike.java

--- src/a10/Mike.java
+++ src/a10/Mike.java
@@ -1,5 +1,43 @@
 package a10;
 
-public class Mike {
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+public class Mike extends Plant{
+
+	private static final int HEALTH = 100;
+	private static final int COOLDOWN = 40;
+	private static final int ATTACKDMG = 20;
+	private static final BufferedImage IMAGE;
+	static {
+		BufferedImage localImage = null;
+		try {
+			localImage = ImageIO.read(new File("src/a10/Icons/mike.png"));
+
+		} catch (IOException e) {
+			System.out.println("Mike image was not found");
+			System.exit(0);
+		}
+		IMAGE = localImage;
+	}
+	
+	public Mike(Double startingPosition) {
+		super(startingPosition, new Point2D.Double(IMAGE.getWidth(), IMAGE.getHeight()), IMAGE, HEALTH, COOLDOWN, ATTACKDMG);
+	}
+	
+	@Override
+	public void attack(Actor other) {
+		if (other instanceof Zombie && this != other && this.isCollidingOther(other)) {
+			if(this.readyForAction()) {
+				other.changeHealth(-ATTACKDMG);
+				this.resetCoolDown();
+			}
+			
+		}
+	}
 }
